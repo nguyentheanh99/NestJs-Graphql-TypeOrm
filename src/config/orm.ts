281daecb16769819f7941ffb9513253314618552
nguyentheanh99 <nguyentheanh10101999@gmail.com>
2022-03-17 11:14:01 +0700

    init

--- src/config/orm.ts
+++ src/config/orm.ts
@@ -2,8 +2,8 @@ import * as path from 'path';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const options: TypeOrmModuleOptions = {
-  type: 'sqlite',
-  database: 'data/rocketseat.db',
+  type: 'postgres',
+  database: 'postgresql-concentric-62156',
   logging: true,
   entities: [path.resolve(__dirname, '..', 'db', 'models', '*')],
   migrations: [path.resolve(__dirname, '..', 'db', 'migrations', '*')],
